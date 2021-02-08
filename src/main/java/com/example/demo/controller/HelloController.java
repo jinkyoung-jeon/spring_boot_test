3c5825a18bdad5014588dc0189cd912f089013eb
jin-kyoung <kyeong1750@gmail.com>
2021-02-08 12:17:24 +0900

    init

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "반갑습니다. 되긴 됐네..";
+    }
+}
